7c4743830e8c69be38df4b24c5734f17396bef22
Abdulkabir Omotoso <kaybeebaba@gmail.com>
2022-03-07 10:55:32 +0100

    Core and Data project added

---
+++ ParkingTicket.Services.Core/Interface/IParkingTicketMain.cs
@@ -0,0 +1,16 @@
+﻿using ParkingTicket.Services.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingTicket.Services.Core.Interface
+{
+    public interface IParkingTicketMain
+    {
+        Task<bool> ComputesAndSaveTicket(AddTicketDto request);
+
+
+    }
+}
---
+++ ParkingTicket.Services.Data/Models/ParkingRule.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingTicket.Services.Data.Models
+{
+    public class ParkingRule
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public Decimal Amount { get; set; }
+    }
+}
